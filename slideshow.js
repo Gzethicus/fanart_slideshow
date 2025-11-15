9fa18c8a4d1b1718ad47c0b30814496d20fb01c8
Gzethicus <94045350+Gzethicus@users.noreply.github.com>
2025-11-15 18:07:53 +0100

    Add image comment

--- slideshow.js
+++ slideshow.js
@@ -1,26 +1,27 @@
 var imgList = [
-	"gallery\\BirbSigmaSketch.png",
-	"gallery\\WRFA_CoralCaves_Fishnoid.png",
-    "gallery\\WARB_Salination_Irri.png",
-    "gallery\\WSKC_StormyCoast_NOTOCORDA.png",
-    "gallery\\WTDA_TorridDesert_headintheklouds.png",
-	"gallery\\WBLA_Badlands_Irri.png",
-
+    {img:"gallery\\BirbSigmaSketch.png",                    comment:"birb"},
+	{img:"gallery\\WBLA_Badlands_Irri.png",                 comment:"Irri"},
+	{img:"gallery\\WRFA_CoralCaves_Fishnoid.png",           comment:"Fishnoid"},
+    {img:"gallery\\WARB_Salination_Irri.png",               comment:"Irri"},
+    {img:"gallery\\WSKC_StormyCoast_NOTOCORDA.png",         comment:"NOTOCORDA"},
+    {img:"gallery\\WTDA_TorridDesert_headintheklouds.png",  comment:"headintheklouds"},
 ];
 var index = 0;
 
 document.addEventListener("DOMContentLoaded", (e) => {
     index = Math.floor(Math.random() * imgList.length);
 
-    document.getElementById("fade").addEventListener("animationiteration", fadeOut);
+    document.getElementById("main").addEventListener("animationiteration", fadeOut);
     fadeOut();
 });
 
 function fadeOut() {
     index++;
     while (index >= imgList.length) index -= imgList.length;
-    document.getElementById("static").src = imgList[index];
+    document.getElementById("static").src = imgList[index].img;
+    document.getElementById("static-label").innerHTML = imgList[index].comment;
     setTimeout(() => {
-        document.getElementById("fade").src = imgList[index];
+        document.getElementById("fade").src = imgList[index].img;
+        document.getElementById("fade-label").innerHTML = imgList[index].comment;
     }, 2000);
 }

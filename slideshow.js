69f8362ea281623fc9e4b3ed53d89bffcf2a5552
Swigma <tropixbruno@gmail.com>
2025-11-28 10:31:28 +0000

    archipelago is frog

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,6 @@
 var imgList = [
     {img:"gallery\\BirbSigmaSketch.png",                    comment:"birb"},
+	{img:"gallery\\WARF_AetherRidge_Cas.png",				comment:"Cas"},
 	{img:"gallery\\WBLA_Badlands_Irri.png",                 comment:"Irri"},
 	{img:"gallery\\WRFA_CoralCaves_Fishnoid.png",           comment:"Fishnoid"},
     {img:"gallery\\WARB_Salination_Irri.png",               comment:"Irri"},

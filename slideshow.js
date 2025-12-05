3b49dc7f9232fd662f51ca1cab1033af9c05ed0f
Swigma <tropixbruno@gmail.com>
2025-12-05 14:23:23 +0000

    Too soon, Executus!

--- slideshow.js
+++ slideshow.js
@@ -1,6 +1,6 @@
 var imgList = [
     {img:"gallery\\BirbSigmaSketch.png",                    comment:"birb"},
-	{img:"WRFB_TurbulentPump_Astroik.png", 					comment:"Astroik"},
+	{img:"gallery\\WRFB_TurbulentPump_Astroik.png", 					comment:"Astroik"},
 	{img:"gallery\\WARE_HeatDucts_RiverTheGremlin.png",		comment:"RiverTheGremlin"},
 	{img:"gallery\\WARF_AetherRidge_Cas.png",				comment:"Cas"},
 	{img:"gallery\\WBLA_Badlands_Irri.png",                 comment:"Irri"},
